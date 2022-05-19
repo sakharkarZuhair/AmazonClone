5e52dc04d86ea87db751cba235141653428409a4
zuhair-credencesoft <zuhair@credencesoft.in>
2022-05-19 04:00:17 -0400

    Step-2 Done. Installed Firebase and Firebase Config File.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA4HqK2CucB3_RQp7djcMiXEsGuhBd0zuU",
+    authDomain: "colone-143c3.firebaseapp.com",
+    projectId: "colone-143c3",
+    storageBucket: "colone-143c3.appspot.com",
+    messagingSenderId: "197430231774",
+    appId: "1:197430231774:web:3bc387cb7b3b922007384f",
+    measurementId: "G-RE7PPV1XJ6",
+};
