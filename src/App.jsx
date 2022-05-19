96fff11cadc2c5e4d5b0f4a2bea240566cf814af
zuhair-credencesoft <zuhair@credencesoft.in>
2022-05-19 05:20:22 -0400

    Step-4 Done. Designed Our App Structure.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./Styles/App.css";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header /> {/* Header */} {/* Home */}{" "}
+    </div>
+  );
+}
+
+export default App;
