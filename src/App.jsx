71ad1862e224e212abbf348cf5fb4632a0b10a75
zuhair-credencesoft <zuhair@credencesoft.in>
2022-05-20 00:00:18 -0400

    Step-6 Done. react-router-dom Installed and Implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./Styles/App.css";
 import Header from "./Components/Header";
 import HomeScreen from "./Screens/HomeScreen";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <HomeScreen /> {/* Header */} {/* Home */}
+      <Routes>
+        <Route path="/" element={((<Header />), (<HomeScreen />))} />
+      </Routes>
     </div>
   );
 }
